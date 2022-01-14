f8d540049fb26f71b0eee24a898fdcd4a8fb6b74
shinakami <30779727+shinakami@users.noreply.github.com>
2022-01-14 14:28:50 +0800

    Update webot.py

--- YT_booster_Python/webot.py
+++ YT_booster_Python/webot.py
@@ -15,6 +15,28 @@ import loguru
 import pyquery
 from tqdm import tqdm
 os.system('cls')
+
+def SSLIPcatcher():
+    response = requests.get("https://www.sslproxies.org/")
+ 
+    proxy_ips = re.findall('\d+\.\d+\.\d+\.\d+:\d+', response.text)  #「\d+」代表數字一個位數以上
+    
+    valid_ips = []
+    for ip in proxy_ips:
+        try:
+            result = requests.get('https://ip.seeip.org/jsonip?',
+			       proxies={'http': ip, 'https': ip},
+			       timeout=5)
+            print(result.json())
+            valid_ips.append(ip)
+        except:
+            print(f"{ip} invalid")
+    return valid_ips
+            
+
+
+
+
 def getProxiesFromFreeProxyList():
     proxies = []
     url = 'https://free-proxy-list.net/'
@@ -26,7 +48,7 @@ def getProxiesFromFreeProxyList():
         return
     loguru.logger.success(f'getProxiesFromFreeProxyList: downloaded.')
     d = pyquery.PyQuery(response.text)
-    trs = list(d('table#proxylisttable > tbody > tr').items())
+    trs = list(d('list > tbody > tr').items())
     loguru.logger.warning(f'getProxiesFromFreeProxyList: scanning...')
     IPPool = []
     for tr in trs:
@@ -39,10 +61,10 @@ def getProxiesFromFreeProxyList():
         # 取出匿名性
         anom = tds[4].text().strip()
         # 組合 IP 代理
-        if anom == 'level3':
-            proxy = f'{anom}:{ip}:{port}'
-            proxies.append(proxy)
-            IPPool.append(pd.DataFrame([{'IP':ip, 'Port':port}]))
+        #if anom == 'level3':
+        proxy = f'{anom}:{ip}:{port}'
+        proxies.append(proxy)
+        IPPool.append(pd.DataFrame([{'IP':ip, 'Port':port}]))
     IPPool = pd.concat(IPPool, ignore_index=True)
     loguru.logger.success(f'getProxiesFromFreeProxyList: scanned.')
     loguru.logger.debug(f'getProxiesFromFreeProxyList: {len(proxies)} level3 proxies is found.')
@@ -52,7 +74,7 @@ def getProxiesFromFreeProxyList():
 
 def IPcheater():
 
-    ActIps = getProxiesFromFreeProxyList()
+    ActIps = IPmoder()
     options = webdriver.ChromeOptions()
     proxy = 'http://'+ActIps['IP'][0]+':'+ActIps['Port'][0]
     options.add_argument(('–proxy-server='+proxy))
@@ -95,6 +117,7 @@ def IPcheater():
                 else:
                     print('Failed: {}:{}'.format(IP, Port), 'code: ', resp.status_code)
             except:
+                    ActIps.append(pd.DataFrame([{'IP':IP[0], 'Port':Port[0]}]))
                     print('Failed: {}:{}'.format(IP, Port), 'code: NULL')
     ActIps = pd.concat(ActIps, ignore_index=True)
     print(ActIps)
@@ -134,25 +157,49 @@ def IPmoder():
 
 #ActIps = IPmoder()
 #ActIps = IPcheater()
-ActIps = getProxiesFromFreeProxyList()
+#ActIps = getProxiesFromFreeProxyList() 
 ops=[]
 ip = []
 ID = []
-for i in range(5):
-    ops.append('')
-    ops[i] = webdriver.ChromeOptions()
-    user = UserAgent()
-    us_a = user.chrome
-    ID.append('')
-    ip.append('')
-    ID[i] = us_a
-    IP_noot = rd.randint(0, len(ActIps['IP'])-1)
-    Port_noot = rd.randint(0, len(ActIps['Port'])-1)
-    ip[i] = ActIps['IP'][IP_noot]+':'+ActIps['Port'][Port_noot]
-    proxy = 'https://'+ActIps['IP'][IP_noot]+':'+ActIps['Port'][Port_noot]
-    ops[i].add_argument('user-agent='+us_a)
-    ops[i].add_argument(('–proxy-server='+proxy))
-    ops[i].add_argument("--mute-audio")
+def IPDataManager():
+    for i in range(5):
+        ops.append('')
+        ops[i] = webdriver.ChromeOptions()
+        user = UserAgent()
+        us_a = user.chrome
+        ID.append('')
+        ip.append('')
+        ID[i] = us_a
+        IP_noot = rd.randint(0, len(ActIps['IP'])-1)
+        Port_noot = rd.randint(0, len(ActIps['Port'])-1)
+        ip[i] = ActIps['IP'][IP_noot]+':'+ActIps['Port'][Port_noot]
+        proxy = 'https://'+ActIps['IP'][IP_noot]+':'+ActIps['Port'][Port_noot]
+        ops[i].add_argument('user-agent='+us_a)
+        ops[i].add_argument(('–proxy-server='+proxy))
+        ops[i].add_argument("--mute-audio")
+
+def SLLIPDataManager(ActIps):
+    for i in range(5):
+        ops.append('')
+        ops[i] = webdriver.ChromeOptions()
+        user = UserAgent()
+        us_a = user.chrome
+        ID.append('')
+        ip.append('')
+        ID[i] = us_a
+        IP_noot = rd.randint(0, len(ActIps)-1)
+        
+        ip[i] = ActIps[IP_noot]
+        proxy = 'https://'+ActIps[IP_noot]
+        ops[i].add_argument('user-agent='+us_a)
+        ops[i].add_argument(('–proxy-server='+proxy))
+        ops[i].add_argument("--mute-audio")
+
+
+ActIps = SSLIPcatcher()
+SLLIPDataManager(ActIps)
+
+
 driver1 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[0])
 os.system('cls')
 driver2 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[1])
@@ -165,68 +212,17 @@ driver5 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[4]
 os.system('cls')
 driver_root = [driver1, driver2, driver3, driver4, driver5]
 os.system('cls')
-net_root1 = ['https://www.youtube.com/watch?v=g78E87yt7fA', 'https://www.youtube.com/watch?v=MxsUinFbKvg' 
-            ,'https://www.youtube.com/watch?v=Nqgw09nnpr8', 'https://www.youtube.com/watch?v=AM9xU9WC7do'
-            ,'https://www.youtube.com/watch?v=ZqcDdqdEOM8', 'https://www.youtube.com/watch?v=4qJPYCRqCNE'
-            ,'https://www.youtube.com/watch?v=PECjdj-qFrE', 'https://www.youtube.com/watch?v=1Zouz94vHMk'
-            ,'https://www.youtube.com/watch?v=FiIL52UD-zM', 'https://www.youtube.com/watch?v=25ClrbBb5fI'
-            ,'https://www.youtube.com/watch?v=cLmF2_g-DIo', 'https://www.youtube.com/watch?v=27bH55gqc7c'
-            ,'https://www.youtube.com/watch?v=ayePHBk6q5M', 'https://www.youtube.com/watch?v=L1x4GGUV0i4'
-            ,'https://www.youtube.com/watch?v=sqJgwUVO_R8', 'https://www.youtube.com/watch?v=sqJgwUVO_R8'
-            ,'https://www.youtube.com/watch?v=JLz9cRxwTZg', 'https://www.youtube.com/watch?v=JLz9cRxwTZg'
-            ,'https://www.youtube.com/watch?v=bDAb8ocgULo', 'https://www.youtube.com/watch?v=bDAb8ocgULo'
-            ,'https://www.youtube.com/watch?v=49X2OfhlZCA', 'https://www.youtube.com/watch?v=49X2OfhlZCA'
-            ,'https://www.youtube.com/watch?v=JIhGw48tv80', 'https://www.youtube.com/watch?v=JIhGw48tv80'
-            ,'https://www.youtube.com/watch?v=JIhGw48tv80', 'https://www.youtube.com/watch?v=JIhGw48tv80'
-            ,'https://www.youtube.com/watch?v=RkXm-nSiu9g', 'https://www.youtube.com/watch?v=N7M2R0vHfx8']
+net_root1 = ['https://www.youtube.com/watch?v=VwWgU6JHuvQ', 'https://www.youtube.com/watch?v=BC2Ulz7896Y'
+            ,'https://www.youtube.com/watch?v=aNo-jPjH5V8', 'https://www.youtube.com/watch?v=v-hKahoiBV0'
+            ,'https://www.youtube.com/watch?v=Ha5g9H4nu6I', 'https://www.youtube.com/watch?v=47xFYqCndLc'
+            ,'https://www.youtube.com/watch?v=D2mMOMbrfQQ', 'https://www.youtube.com/watch?v=W-oZxYwTscM']
 
 
 net = [net_root1]
 time_sleep = 0
 
 while True :
-
-
-    os.system('cls')
-    if time_sleep == 2077:
-        for driver_boot in driver_root:
-            driver_boot.delete_all_cookies()
-        sleep_time = rd.randint(10, 600)
-        print('sleeping time baby!:',sleep_time)
-        sleep(sleep_time)
-        time_sleep = 0
-        ActIps = IPmoder()
-        #ActIps = IPcheater()
-        ops=[]
-        ip = []
-        ID = []
-        for i in range(5):
-            ops.append('')
-            ops[i] = webdriver.ChromeOptions()
-            user = UserAgent()
-            us_a = user.chrome
-            ID.append('')
-            IP_noot = rd.randint(0, len(ActIps['IP'])-1)
-            Port_noot = rd.randint(0, len(ActIps['Port'])-1)
-            ip[i] = ActIps['IP'][IP_noot]+':'+ActIps['Port'][Port_noot]
-            ID[i] = us_a
-            proxy = 'https://'+ActIps['IP'][IP_noot]+':'+ActIps['Port'][Port_noot]
-            ops[i].add_argument('user-agent='+us_a)
-            ops[i].add_argument(('–proxy-server='+proxy))
-            ops[i].add_argument("--mute-audio")
-        driver1 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[0])
-        os.system('cls')
-        driver2 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[1])
-        os.system('cls')
-        driver3 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[2])
-        os.system('cls')
-        driver4 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[3])
-        os.system('cls')
-        driver5 = webdriver.Chrome(executable_path='chromedriver', chrome_options=ops[4])
-        os.system('cls')
-        driver_root = [driver1, driver2, driver3, driver4, driver5]
-        os.system('cls')
-
+   
     os.system('cls')
     for i in range(5):    
         print(ID[i])
@@ -234,7 +230,7 @@ while True :
         sleep(1)
     nt = 0
     time_sleep = time_sleep + 1
-    time = 480
+    time = 360
     net_root = net[nt]
     ch = rd.randint(0, len(net_root)-1)
     ch2 = rd.randint(0, len(net_root)-1)
@@ -261,7 +257,7 @@ while True :
         for i in range(time):
             sleep(1)
             tbar.update(1)
-    #sleep(time)
+    
     for driver_boot in driver_root:
         driver_boot.delete_all_cookies()
         driver_boot.refresh()
