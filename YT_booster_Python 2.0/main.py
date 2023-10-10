13c1c8279a33685ec6f87176d7a4222974568cf9
PYKAMIA <30779727+shinakami@users.noreply.github.com>
2023-10-10 20:22:20 +0800

    Update main.py

--- YT_booster_Python 2.0/main.py
+++ YT_booster_Python 2.0/main.py
@@ -2,6 +2,9 @@ import webot
 
 if __name__ == '__main__':
     print("Main program Start")
-    minimum = 5
-    listValidIP = webot.SSLIPcatcher(minimum_ipcount=minimum)
-    webot.Exacute(listValidIP)
+    minimum_ip = 3
+    execute_time = 5
+    max_step = 3
+    listValidIP = webot.SSLIPcatcher(minimum_ipcount=minimum_ip)
+    print(listValidIP)
+    webot.Execute(listValidIP, execute_time, max_step)
