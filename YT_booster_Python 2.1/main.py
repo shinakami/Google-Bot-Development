05f3bff69bee21851592d4e768815ac2d2815a77
PYKAMIA <30779727+shinakami@users.noreply.github.com>
2024-10-18 13:11:51 +0800

    Update main.py

--- YT_booster_Python 2.1/main.py
+++ YT_booster_Python 2.1/main.py
@@ -4,8 +4,17 @@ if __name__ == '__main__':
     os.system("cls")
     print("Main program Start")
     
-    minimum_ip = 5
+    minimum_ip = 7
     execute_time = 180
-    execute_step = 3
+    execute_step = 5
 
-    webot.Execute(minimum_ip, execute_time, execute_step)
+
+    address = ['https://www.youtube.com/watch?v=muNCt2rkH3w', 'https://www.youtube.com/watch?v=vn-ze9UZX2c'
+           ,'https://www.youtube.com/watch?v=13Y7JvsgplI', 'https://www.youtube.com/watch?v=iXbBTLAZ4jY'
+           ,'https://www.youtube.com/watch?v=yc7Li_LLofk', 'https://www.youtube.com/watch?v=Qdt37PmrBdY'
+           ,'https://www.youtube.com/watch?v=-2xVWMFF3dY', 'https://www.youtube.com/watch?v=jH9MRpaJIn8'
+           ,'https://www.youtube.com/watch?v=q018elKqDws', 'https://www.youtube.com/watch?v=YmlyE94vYqU'
+           ,'https://www.youtube.com/watch?v=orXrSx5L0k4', 'https://www.youtube.com/watch?v=sy95ABSMnPE'
+           ,'https://www.youtube.com/watch?v=Ju5U3mZJYYc', 'https://www.youtube.com/watch?v=3TKABB3mUNA']
+
+    webot.Execute(minimum_ip, execute_time, execute_step, address)
